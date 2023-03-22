a6e2913739959093280e17c8eb0dbf16721064e8
Sapodsoblyaeva <sa.podsoblyaeva@yandex.ru>
2023-03-22 18:32:08 +0300

    3/5

---
+++ Lab 1.3_nextCmd.py
@@ -0,0 +1,9 @@
+from pysnmp.hlapi import *
+
+obj_ver = ObjectIdentity("1.3.6.1.2.1.2.2.1.2")
+
+result = nextCmd(SnmpEngine(), CommunityData("public", mpModel=0), UdpTransportTarget(("10.31.70.209", 161)), ContextData(), ObjectType(obj_ver), lexicographicMode=False)
+
+for received_data in result:
+	for x in received_data[3]:
+		print(x)
