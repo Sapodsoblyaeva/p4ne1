684121ad027df76ea3a1bed44770eeb1c2d1e0b8
Sapodsoblyaeva <sa.podsoblyaeva@yandex.ru>
2023-03-23 16:46:24 +0300

    3/4

---
+++ Lab 1.4.py
@@ -0,0 +1,22 @@
+import ipaddress #ip1 = ipaddress.IPv4Address('192.168.1.0')
+import random
+
+network_list = []
+for n in range(0, 50):
+    net = random.randint(0x0B000000, 0xDF000000)
+    mask = random.randint(8, 24)
+    ipaddress.IPv4Network((net, mask), strict=False)
+    n1 = ipaddress.IPv4Network((net, mask), strict=False)
+    network_list.append(n1)
+
+print(sorted(network_list))
+
+
+
+
+
+
+
+
+
+
