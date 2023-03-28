b17cfd0bfb8424d27c0f4f8ac181ef0ae23b4a05
Sapodsoblyaeva <sa.podsoblyaeva@yandex.ru>
2023-03-28 18:26:28 +0300

    3/4

--- Lab_2.2restapi.py
+++ Lab_2.2restapi.py
@@ -10,18 +10,30 @@ from requests.adapters import HTTPAdapter
 
 class Ssl1HttpAdapter(HTTPAdapter):
     def init_poolmanager(self, connections, maxsize, block=False):
-        self.poolmanager = PoolManager(num_pools=connections, maxsize=maxsize, block=block, ssl_version=ssl.PROTOCOL_TLSv1)
+        self.poolmanager = PoolManager(
+            num_pools=connections,
+            maxsize=maxsize,
+            block=block,
+            ssl_version=ssl.PROTOCOL_TLSv1
+        )
 
 requests.packages.urllib3.util.ssl_.DEFAULT_CIPHERS = 'ALL:@SECLEVEL=1'
 
 s = requests.Session()
 s.mount("https://10.31.70.210:55443", Ssl1HttpAdapter())
 
-r = s.post("https://10.31.70.210:55443/api/v1/auth/token-services",auth=("restapi", "j0sg1280-7@"), verify=False)
+r = s.post(
+    "https://10.31.70.210:55443/api/v1/auth/token-services",
+    auth=("restapi", "j0sg1280-7@"),
+    verify=False
+)
 
 token = r.json()['token-id']
 header = {"content-type": "application/json", "X-Auth-Token": token}
-r = s.get("https://10.31.70.210:55443/api/v1/interfaces", headers=header, verify=False)
-print(r.status_code)
+r = s.get(
+    "https://10.31.70.210:55443/api/v1/interfaces",
+    headers=header,
+    verify=False
+)
 pprint.pprint(r.json())
 
