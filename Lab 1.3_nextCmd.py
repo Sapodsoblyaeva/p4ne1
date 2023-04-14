062301ceda7bd178fbd42af064469fcece62c483
sveta <sa.podsoblyaeva@yandex.ru>
2023-04-14 12:36:42 +0300

    1

--- Lab 1.3_nextCmd.py
+++ Lab 1.3_nextCmd.py
@@ -2,8 +2,17 @@ from pysnmp.hlapi import *
 
 obj_ver = ObjectIdentity("1.3.6.1.2.1.2.2.1.2")
 
-result = nextCmd(SnmpEngine(), CommunityData("public", mpModel=0), UdpTransportTarget(("10.31.70.209", 161)), ContextData(), ObjectType(obj_ver), lexicographicMode=False)
+result = nextCmd(SnmpEngine(),
+				 CommunityData("public", mpModel=0),
+				 UdpTransportTarget(("10.31.70.209", 161)),
+				 ContextData(),
+				 ObjectType(obj_ver),
+				 lexicographicMode=False)
 
-for received_data in result:
-	for x in received_data[3]:
-		print(x)
+def interfaces():
+	for received_data in result:
+		for x in received_data[3]:
+			yield x
+
+for i in interfaces():
+	print(i)
